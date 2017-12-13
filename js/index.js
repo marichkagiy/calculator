cd4a50bbf9da173bc4c80f7ad2e7986b640ce303
marichkagiy <fedevych.m@gmail.com>
2017-12-13 16:40:51 +0000

    Inintial

---
+++ js/index.js
@@ -0,0 +1,18 @@
+ let inputLabel = document.getElementById('inputLabel');
+     
+    function pushBtn(obj) {
+         
+        let pushed = obj.innerHTML;
+         
+        if (pushed == '=') {
+            inputLabel.innerHTML = eval(inputLabel.innerHTML);
+        } else if (pushed == 'AC') {
+            inputLabel.innerHTML = '0';
+        } else {
+                if (inputLabel.innerHTML == '0') {
+                inputLabel.innerHTML = pushed;
+        } else {
+                inputLabel.innerHTML += pushed;
+            }
+        }
+    }
